e4fa7d0f24b93a571c0c949d4288085c591cc24a
davidOlesinski <117949182+davidOlesinski@users.noreply.github.com>
2022-12-11 21:07:29 +0100

    Add files via upload

---
+++ JO2_byte_short_int_long.java
@@ -0,0 +1,14 @@
+package com.example.basics.data;
+
+public class JO2_byte_short_int_long
+{
+    public static void main(String args[])
+    {
+        byte small = -127;
+        short numShort = - 32000;
+        int number = 32341;
+        long big = 4234234;
+
+        System.out.println(number);
+    }
+}
---
+++ JO3_floatDouble.java
@@ -0,0 +1,12 @@
+package com.example.basics.data;
+
+public class JO3_floatDouble
+{
+    public static void main(String args[])
+    {
+        float nubmer = 102.5f;
+        double bigNumber = 213.45323434;
+
+        System.out.println("Number: " + nubmer + " bigNumber: " + bigNumber);
+    }
+}
